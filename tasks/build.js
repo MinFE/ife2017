cf9a0275785f8071289f00461ba8b7681d219f98
minfive <chenxiaowu1994@outlook.com>
2017-02-25 17:57:19 +0800

    基本环境搭建

---
+++ tasks/build.js
@@ -0,0 +1,24 @@
+require('shelljs/global');
+
+const 
+	path = require('path'),
+	webpack = require('webpack'),
+	webpackMerge = require('webpack-merge');
+
+const baseConfig = require('../webpack.config.base.js');
+
+const 
+	argvs = process.argv,
+	reg = '--src:';
+
+const _src = argvs.reduce((p, argv) => {
+	return argv.match(reg) ? argv.replace(reg, '') : p;
+}, '');
+
+let config = require(path.join('..', _src, 'webpack.config.js'));
+
+rm('-rf', path.join(_src, 'dist'));
+
+webpack(webpackMerge(baseConfig, config), (err, stats) => {
+	console.log(stats);
+});
---
+++ webpack.config.base.js
@@ -0,0 +1,70 @@
+const 
+	webpack = require('webpack');
+
+const 
+	ExtractTextPlugin = require('extract-text-webpack-plugin'),
+	HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.(sass|scss)$/,
+				exclude: /node_modules/,
+				use: ExtractTextPlugin.extract({
+					use: [
+						'css-loader',
+						'sass-loader'
+					]
+				})
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [{
+					loader: 'babel-loader',
+					options: {
+						presets: ['es2015', 'stage-2'],
+						plugins: ['transform-runtime'],
+						cacheDirectory: true
+					}
+				}]
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				exclude: /node_modules/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						limit: 10000,
+						name: 'images/[name].[hash:8].[ext]'
+					}
+				}
+			}
+		]
+	},
+
+	plugins: [
+		// 版权插件
+		new webpack.BannerPlugin('<author: minfive; github: https://github.com/Mrminfive>'),
+
+		// js压缩
+		// new webpack.optimize.UglifyJsPlugin({
+		// 	sourceMap: true
+		// }),
+
+		// 提取公用代码
+		new webpack.optimize.CommonsChunkPlugin({
+			name: ['three', 'manifest'],
+			minChunk: function(module) {
+				return module.context && module.context.indexOf('node_moudles') !== -1;
+			}
+		}),
+
+		// 提取css
+		new ExtractTextPlugin({
+			filename: 'styles.css'
+		})
+	]
+}
